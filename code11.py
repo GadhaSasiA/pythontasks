6aafe10df4cf30904ac1d72d480704803913457c
GadhaSasiA <144037281+GadhaSasiA@users.noreply.github.com>
2024-04-26 20:39:47 +0530

    Add files via upload

---
+++ code11.py
@@ -0,0 +1,46 @@
+# my_list = [1, 2, 3, 4]
+# my_list.append(5)  
+# print(my_list) 
+
+# my_tuple = (1, 2, 3)
+# print(my_tuple)  
+
+# my_set = {1, 2, 3, 1} 
+# print(my_set)  
+
+# my_dict = {"name": "John", "age": 30}
+# my_dict["city"] = "New York"  
+# print(my_dict) 
+
+# # If-Else
+# number = 10
+
+# if number > 5:
+#     print("Greater than 5")
+# else:
+#     print("Less than or equal to 5")
+
+# for i in range(1, 6):
+#     print(i) 
+
+# count = 1
+# while count <= 3:
+#     print(count)  
+#     count += 1
+
+# Function definition
+def add(a, b):
+    return a + b
+
+result = add(3, 5)
+print(result)
+
+my_list = [1, "Hello", 3.5, True]
+
+print(my_list[1]) 
+
+my_list.append("Python")
+print(my_list) 
+
+for item in my_list:
+    print(item)
